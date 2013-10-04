1256cb066cffa0c9cdb8af32efb3c721a24ef659
Kyle Berg <kyle.w.berg@gmail.com>
2013-10-04 00:25:02 -0500

    removed ams message character count, added phone numbers to list items and adjusted layout, adjusted layout to accommodate all voicemail icons, added app move (drag) functionality

--- js/application.js
+++ js/application.js
@@ -12,7 +12,7 @@ var App = {
 		"full" : "2.0.0.0"
 	}
  },
- "frames" : {
+ "frame" : {
  	"current" : null,
 	"previous" : null
  },
@@ -96,10 +96,7 @@ var App = {
 
 /*--Init------------*/
 
-App.initialize = function() {		
-	//load data
-	this.loadData();
-	
+App.initialize = function() {			
 	//init plugins
 	this.initPlugins();
 	
@@ -109,6 +106,9 @@ App.initialize = function() {
 	//enable modes
 	//this.enableDevelopment();
 	//this.enableCommercial();	
+	
+	//data
+	this.loadData();
 	
 	//navigate to start frame
 	this.navigate("login");	
@@ -202,7 +202,7 @@ App.changeActiveNumber = function(){
 
 App.login = function(){
 	this.states.loggedIn = true;
-	$(this.elements.app).addClass("logged-in");		
+	$(this.elements.app).addClass("logged-in");			
 	this.collapse();
 }
 
@@ -274,7 +274,7 @@ App.navigate = function(frame){
 }
 
 App.navigatePrevious = function(){
-	this.navigate(this.frames.previous);
+	this.navigate(this.frame.previous);
 }
 
 /*--Frames------------*/
@@ -283,13 +283,13 @@ App.hideFrames = function(){
 	$(this.elements.frame).hide();
 }
 
-App.showFrame = function(frame){
-	this.log("show frame: "+frame);
-	this.frames.previous = App.frames.current;
-	this.frames.current = frame;
+App.showFrame = function(frameReference){
+	this.log("show frame: "+frameReference);
+	this.frame.previous = this.frame.current;
+	this.frame.current = frameReference;
 	this.expand();
 	this.hideFrames();
-	$(this.elements.frames[frame]).show();	
+	$(this.elements.frames[frameReference]).show();	
 }
 
 /*--Data------------*/
@@ -303,7 +303,7 @@ App.loadRingtones = function(n){
 App.loadData = function(){
 	Calls.load(50);
 	Contacts.load(10);		
-	Voicemails.load(5);
+	Voicemails.load(50);
 	Conversations.load(10);
 	Messages.load(10);
 	this.loadRingtones(20);					
@@ -314,9 +314,6 @@ App.loadData = function(){
 /*--App Event Registration------------*/
 $(document).ready(function(){
 	App.initialize();
-	
-	//app functionality
-	$(App.elements.top).onmousedown = function(event) {App.move();}
 	
 	//select dropdowns
 	$(App.elements.selectMenus.activeNumber).on("change", function(){App.changeActiveNumber();});	
@@ -359,212 +356,9 @@ $(document).ready(function(){
 	
 	//expandable containers
 	$(App.elements.expandableContainer.title).click(function(){$(this).parent().toggleClass("open");});	
-});
-
-
-
-/*--SMS Conversations Frame------------*/
-
-App.conversations = {
-	"buttons" : {
-		"new" : "#new-sms-button"
-	}
-}
-
-$(document).ready(function(){
-	$(App.conversations.buttons["new"]).click(function(){App.navigate("sms-conversation-new");});
-});
-
-/*--SMS Conversation Frame------------*/
-
-App.conversation = {
-	"frame" : App.elements.frames["sms-conversation"],
-	"backLink" : "#sms-back",
-	"buttons" : {
-		"edit" : "#edit-conversation-button",
-		"cancel" : "#cancel-delete-button",
-		"deleteSelected" : "#delete-selected-button",
-		"deleteAll" : "#delete-all-button",
-		"send" : "#send-message-button"
-	},
-	"inputs" : {
-		"deleteCheckbox" : ".delete-message",
-		"messageContent" : "#message-content"
-	},
-	"outputs" : {
-		"count" : "#character-count"
-	}
-}
-
-App.conversation.enableDeleteMode = function(){
-	$(this.frame).addClass("delete-mode");	
-	this.updateDeleteButton();
-}
-
-App.conversation.disableDeleteMode = function(){
-	$(this.frame).removeClass("delete-mode");	
-	$(this.inputs.deleteCheckbox).prop('checked', false);
-	this.updateDeleteButton();
-}
-
-App.conversation.updateDeleteButton = function(){
-	var n = $(this.inputs.deleteCheckbox+':checked').length;
-	$(this.buttons.deleteSelected).html("Delete Selected ("+n+")");
-}
-
-App.conversation.edit = function(){
-	this.enableDeleteMode();
-}
-
-App.conversation.cancel = function(){
-	this.disableDeleteMode();
-}
-
-App.conversation.deleteSelected = function(){
-	this.disableDeleteMode();
-}
-
-App.conversation.deleteAll = function(){
-	App.navigate("sms-conversations");
-	this.disableDeleteMode();
-}
-
-App.conversation.back = function(){
-	App.navigate("sms-conversations");
-	this.disableDeleteMode();
-}
-
-App.conversation.updateCharacterCount = function(){
-	var n = $(this.inputs.messageContent).val().length;
-	$(this.outputs.count).html(n+"/160");
-}
-
-App.conversation.resetSendMessage = function(){
-	$(this.inputs.messageContent).val("");	
-	this.updateCharacterCount();
-}
-
-App.conversation.sendMessage = function(){
-	var n = $(this.inputs.messageContent).val().length;	
-	if(n > 0){
-		Messages.load(1);
-		this.resetSendMessage();
-	}
-}
-
-$(document).ready(function(){
-	$(App.conversation.backLink).click(function(){App.conversation.back();});
-	$(App.conversation.buttons.send).click(function(){App.conversation.sendMessage();});
-	$(App.conversation.buttons.edit).click(function(){App.conversation.edit();});
-	$(App.conversation.buttons.cancel).click(function(){App.conversation.cancel();});
-	$(App.conversation.buttons.deleteSelected).click(function(){App.conversation.deleteSelected();});
-	$(App.conversation.buttons.deleteAll).click(function(){App.conversation.deleteAll();});		
-	$(App.conversation.inputs.deleteCheckbox).on("change", function(){App.conversation.updateDeleteButton();});
-	$(App.conversation.inputs.messageContent).on("input propertychange", function(){App.conversation.updateCharacterCount();});	
-});
-
-/*--New SMS Conversation Frame------------*/
-
-App.newConversation = {
-	"frame" : App.elements.frames["sms-conversation-new"],
-	"backLink" : "#sms-new-back",
-	"listItem" : ".recipient",
-	"buttons" : {
-		"cancel" : "#cancel-new-message-button",
-		"send" : "#send-new-message-button",
-		"add" : "#add-recipient-button",
-		"cancelRecipient" :  "#cancel-recipient-button"
-	},
-	"inputs" : {
-		"messageContent" : "#new-message-content",
-		"recipientFilter" : "#recipient-filter"
-	},
-	"outputs" : {
-		"count" : "#new-character-count",
-		"list" : "#recipient-list",
-		"recipient" : "#recipient-label"
-	},
-	"recipient" : false
-}
-
-App.newConversation.cancel = function(){
-	App.navigate("sms-conversations");
-	this.resetSendMessage();
-}
-
-App.newConversation.back = function(){
-	App.navigate("sms-conversations");
-	this.resetSendMessage();
-}
-
-App.newConversation.updateCharacterCount = function(){
-	var n = $(this.inputs.messageContent).val().length;
-	$(this.outputs.count).html(n+"/160");
-}
-
-App.newConversation.resetSendMessage = function(){
-	$(this.inputs.messageContent).val("");	
-	this.updateCharacterCount();
-	this.resetAddRecipient();
-	this.clearRecipient();
-}
-
-App.newConversation.sendMessage = function(){
-	var n = $(this.inputs.messageContent).val().length;	
-	if(n > 0 && this.recipient){
-		App.navigate("sms-conversation");
-		this.resetSendMessage();
-	}
-}
-
-App.newConversation.enableAddRecipient = function(n){
-	$(this.frame).addClass("add-recipient");
-	this.loadRecipients(5);
-}
-
-App.newConversation.loadRecipients = function(n){
-	this.removeRecipients();
-	Recipients.load(n);
-}
-
-App.newConversation.filterRecipients = function(){
-	this.removeRecipients();
-	var filter = $(this.inputs.recipientFilter).val();
-	var length = filter.length;
-	var n = (length <= 5) ? 5 - length : 0;
-	Recipients.load(n);
-}
-
-App.newConversation.addRecipient = function(){
-	this.recipient = "1234567890";
-	$(this.outputs.recipient).html("123-456-7890");
-	this.resetAddRecipient();
-}
-
-App.newConversation.resetAddRecipient = function(){
-	$(this.frame).removeClass("add-recipient");	
-	this.removeRecipients();
-	$(this.inputs.recipientFilter).val("");
-}
-
-App.newConversation.clearRecipient = function(){
-	this.recipient = false;
-	$(this.outputs.recipient).html("");
-}
-
-App.newConversation.removeRecipients = function(){
-	$(this.outputs.list).empty();	
-}
-
-$(document).ready(function(){
-	$(App.newConversation.backLink).click(function(){App.newConversation.back();});	
-	$(App.newConversation.buttons.add).click(function(){App.newConversation.enableAddRecipient();});
-	$(App.newConversation.buttons.cancelRecipient).click(function(){App.newConversation.resetAddRecipient();});	
-	$(App.newConversation.buttons.send).click(function(){App.newConversation.sendMessage();});
-	$(App.newConversation.buttons.cancel).click(function(){App.newConversation.cancel();});
-	$(document).on("click", App.newConversation.listItem, function(){App.newConversation.addRecipient();});
-	$(App.newConversation.inputs.messageContent).on("input propertychange", function(){App.newConversation.updateCharacterCount();});	
-	$(App.newConversation.inputs.recipientFilter).on("input propertychange", function(){App.newConversation.filterRecipients();});
+	
+	//app functionality
+	$(App.elements.top).on("mousedown", function(){App.move();});		
 });
 
 /*--Modal------------*/
---
+++ js/controllers.js
@@ -0,0 +1,352 @@
+var testContent = {
+	"contact" : {
+		"name" : "Contact Name",
+		"number" : "000-000-0000",
+		"type" : "Number Type"
+	},
+	"identifier" : {
+		"call" : "Call Identifier",
+		"voicemail" : "Voicemail Identifier"
+	}
+}
+
+/*--Calls Frame------------*/
+
+
+
+
+var Calls = {
+	"container" : "#call-list",
+	"template" : "#templates .call"
+};
+
+Calls.load = function(n){
+	var contact, clone;
+	for (var i = n - 1; i >= 0; i--){
+		contact = (Math.random() > 0.5) ? true : false;
+		clone = $(this.template).clone();
+		if(contact){
+			clone.find('.name').html(testContent.contact.name);	
+			clone.find('.number').html(testContent.contact.type);	
+		}else{
+			clone.find('.name').html(testContent.identifier.call);	
+			clone.find('.number').html(testContent.contact.number);			
+		}
+		$(this.container).append(clone);
+	};	
+}
+
+
+/*--Contacts------------*/
+
+var Contacts = {
+	"container" : "#contact-list",
+	"template" : "#templates .contact"
+};
+
+Contacts.load = function(n){
+	var state, clone;
+	for (var i = n - 1; i >= 0; i--){
+		clone = $(this.template).clone();
+		clone.find('.name').html(testContent.contact.name);	
+		$(this.container).append(clone);
+	};	
+}
+
+/*--Voicemails------------*/
+
+var Voicemails = {
+	"container" : "#voicemail-list",
+	"template" : "#templates .voicemail"
+};
+
+Voicemails.load = function(n){
+	var contact, unread, urgent, confidential, transcript, clone;
+	var content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat lacus, at tincidunt ante. Vivamus neque velit, lobortis vel sapien viverra fusce."	
+	for (var i = n - 1; i >= 0; i--){
+		contact = (Math.random() > 0.5) ? true : false;
+		unread = (Math.random() > 0.7) ? true : false;
+		urgent = (Math.random() > 0.9) ? true : false;
+		confidential = (Math.random() > 0.9) ? true : false;
+		transcript = true;
+		clone = $(this.template).clone();
+		if(contact){
+			clone.find('.name').html(testContent.contact.name);	
+			clone.find('.number').html(testContent.contact.type);	
+		}else{
+			clone.find('.name').html(testContent.identifier.voicemail);	
+			clone.find('.number').html(testContent.contact.number);			
+		}
+		if(unread){
+			clone.addClass("unread");
+		}
+		if(urgent){
+			clone.addClass("urgent");
+		}
+		if(confidential){
+			clone.addClass("confidential");
+		}
+		if(transcript){
+			clone.addClass("has-copy");
+			clone.find('.copy').html(content);			
+		}
+		$(this.container).append(clone);
+	};	
+}
+
+/*--Conversations------------*/
+
+var Conversations = {
+	"container" : "#conversation-list",
+	"template" : "#templates .conversation"
+};
+
+Conversations.load = function(n){
+	var contact, clone;
+	for (var i = n - 1; i >= 0; i--){
+		contact = (Math.random() > 0.5) ? true : false;
+		clone = $(this.template).clone();
+		if(contact){
+			clone.find('.name').html(testContent.contact.name);	
+			clone.find('.number').html(testContent.contact.type);	
+		}else{
+			clone.find('.name').html(testContent.contact.number);	
+			clone.find('.number').html("");			
+		}
+		$(this.container).append(clone);
+	};	
+}
+
+
+/*--Messages------------*/
+
+var Messages = {
+	"container" : "#message-list",
+	"template" : "#templates .message"
+};
+
+Messages.load = function(n){
+	var state, length, clone;
+	var content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed placerat lacus, at tincidunt ante. Vivamus neque velit, lobortis vel sapien viverra fusce."
+	var contact = (Math.random() > 0.5) ? true : false;
+	for (var i = n - 1; i >= 0; i--){
+		state = (Math.random() > 0.5) ? "incoming" : "outgoing";
+		length = Math.floor((Math.random()*160)+1);
+		clone = $(this.template).clone();
+		clone.addClass(state);
+		clone.find('.copy').html(content.substring(0, length));
+		if(contact && state == "incoming"){
+			clone.find('.name').html(testContent.contact.name);	
+		}else if(state == "outgoing"){
+			clone.find('.name').html("123-456-7890");	
+		}
+		$(this.container).append(clone);
+	};	
+}
+
+/*--SMS Conversations Frame------------*/
+
+App.conversations = {
+	"buttons" : {
+		"new" : "#new-sms-button"
+	}
+}
+
+$(document).ready(function(){
+	$(App.conversations.buttons["new"]).click(function(){App.navigate("sms-conversation-new");});
+});
+
+/*--SMS Conversation Frame------------*/
+
+App.conversation = {
+	"frame" : App.elements.frames["sms-conversation"],
+	"backLink" : "#sms-back",
+	"buttons" : {
+		"edit" : "#edit-conversation-button",
+		"cancel" : "#cancel-delete-button",
+		"deleteSelected" : "#delete-selected-button",
+		"deleteAll" : "#delete-all-button",
+		"send" : "#send-message-button"
+	},
+	"inputs" : {
+		"deleteCheckbox" : ".delete-message",
+		"messageContent" : "#message-content"
+	},
+	"outputs" : {
+		"deleteSelectedCount" : "#delete-selected-count"
+	}
+}
+
+App.conversation.enableDeleteMode = function(){
+	$(this.frame).addClass("delete-mode");	
+	this.updateDeleteButton();
+}
+
+App.conversation.disableDeleteMode = function(){
+	$(this.frame).removeClass("delete-mode");	
+	$(this.inputs.deleteCheckbox).prop('checked', false);
+	this.updateDeleteButton();
+}
+
+App.conversation.updateDeleteButton = function(){
+	var n = $(this.inputs.deleteCheckbox+':checked').length;
+	$(this.outputs.deleteSelectedCount).html(n);
+}
+
+App.conversation.edit = function(){
+	this.enableDeleteMode();
+}
+
+App.conversation.cancel = function(){
+	this.disableDeleteMode();
+}
+
+App.conversation.deleteSelected = function(){
+	this.disableDeleteMode();
+}
+
+App.conversation.deleteAll = function(){
+	App.navigate("sms-conversations");
+	this.disableDeleteMode();
+}
+
+App.conversation.back = function(){
+	App.navigate("sms-conversations");
+	this.disableDeleteMode();
+}
+
+App.conversation.resetSendMessage = function(){
+	$(this.inputs.messageContent).val("");	
+}
+
+App.conversation.sendMessage = function(){
+	var n = $(this.inputs.messageContent).val().length;	
+	if(n > 0){
+		Messages.load(1);
+		this.resetSendMessage();
+	}
+}
+
+$(document).ready(function(){
+	$(App.conversation.backLink).click(function(){App.conversation.back();});
+	$(App.conversation.buttons.send).click(function(){App.conversation.sendMessage();});
+	$(App.conversation.buttons.edit).click(function(){App.conversation.edit();});
+	$(App.conversation.buttons.cancel).click(function(){App.conversation.cancel();});
+	$(App.conversation.buttons.deleteSelected).click(function(){App.conversation.deleteSelected();});
+	$(App.conversation.buttons.deleteAll).click(function(){App.conversation.deleteAll();});		
+	$(App.conversation.inputs.deleteCheckbox).on("change", function(){App.conversation.updateDeleteButton();});
+});
+
+/*--New SMS Frame------------*/
+
+var NewSMSFrame = {
+	"frame" : "#sms-conversation-new",
+	"backLink" : "#sms-new-back",
+	"listItem" : ".recipient",
+	"buttons" : {
+		"cancel" : "#cancel-new-message-button",
+		"send" : "#send-new-message-button",
+		"add" : "#add-recipient-button",
+		"cancelRecipient" :  "#cancel-recipient-button"
+	},
+	"inputs" : {
+		"messageContent" : "#new-message-content",
+		"recipientFilter" : "#recipient-filter"
+	},
+	"outputs" : {
+		"list" : "#recipient-list",
+		"recipient" : "#recipient-label"
+	},
+	"recipient" : false
+}
+
+NewSMSFrame.cancel = function(){
+	App.navigate("sms-conversations");
+	this.resetSendMessage();
+}
+
+NewSMSFrame.back = function(){
+	App.navigate("sms-conversations");
+	this.resetSendMessage();
+}
+
+NewSMSFrame.resetSendMessage = function(){
+	$(this.inputs.messageContent).val("");	
+	this.resetAddRecipient();
+	this.clearRecipient();
+}
+
+NewSMSFrame.sendMessage = function(){
+	var n = $(this.inputs.messageContent).val().length;	
+	if(n > 0 && this.recipient){
+		App.navigate("sms-conversation");
+		this.resetSendMessage();
+	}
+}
+
+NewSMSFrame.enableAddRecipient = function(n){
+	$(this.frame).addClass("add-recipient");
+	this.loadRecipients(5);
+}
+
+NewSMSFrame.loadRecipients = function(n){
+	this.removeRecipients();
+	Recipients.load(n);
+}
+
+NewSMSFrame.filterRecipients = function(){
+	this.removeRecipients();
+	var filter = $(this.inputs.recipientFilter).val();
+	var length = filter.length;
+	var n = (length <= 5) ? 5 - length : 0;
+	Recipients.load(n);
+}
+
+NewSMSFrame.addRecipient = function(){
+	this.recipient = "1234567890";
+	$(this.outputs.recipient).html("123-456-7890");
+	this.resetAddRecipient();
+}
+
+NewSMSFrame.resetAddRecipient = function(){
+	$(this.frame).removeClass("add-recipient");	
+	this.removeRecipients();
+	$(this.inputs.recipientFilter).val("");
+}
+
+NewSMSFrame.clearRecipient = function(){
+	this.recipient = false;
+	$(this.outputs.recipient).html("");
+}
+
+NewSMSFrame.removeRecipients = function(){
+	$(this.outputs.list).empty();	
+}
+
+$(document).ready(function(){
+	$(NewSMSFrame.backLink).click(function(){NewSMSFrame.back();});	
+	$(NewSMSFrame.buttons.add).click(function(){NewSMSFrame.enableAddRecipient();});
+	$(NewSMSFrame.buttons.cancelRecipient).click(function(){NewSMSFrame.resetAddRecipient();});	
+	$(NewSMSFrame.buttons.send).click(function(){NewSMSFrame.sendMessage();});
+	$(NewSMSFrame.buttons.cancel).click(function(){NewSMSFrame.cancel();});
+	$(document).on("click", NewSMSFrame.listItem, function(){NewSMSFrame.addRecipient();});
+	$(NewSMSFrame.inputs.recipientFilter).on("input propertychange", function(){NewSMSFrame.filterRecipients();});
+});
+
+/*--Recipients------------*/
+
+var Recipients = {
+	"container" : "#recipient-list",
+	"template" : "#templates .recipient"
+};
+
+Recipients.load = function(n){
+	var clone;
+	for (var i = n - 1; i >= 0; i--){
+		clone = $(this.template).clone();
+		clone.find('.name').html(testContent.contact.name);	
+		clone.find('.number').html(testContent.contact.number);
+		clone.find('.type').html(testContent.contact.number);
+		$(this.container).append(clone);
+	};	
+}
